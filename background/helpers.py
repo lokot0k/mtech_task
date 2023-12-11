39b81682194c8e7cfaf22a9a82faa1f396b80677
Локоток <kaptoxa322@mail.ru>
2023-12-12 02:33:54 +0300

    chore: proper comments about lockfile

--- background/helpers.py
+++ background/helpers.py
@@ -7,16 +7,17 @@ def wait_for_file_release(lockfile: str) -> bool:
     while os.path.exists(lockfile):
         time.sleep(0.1)
     try:
-        os.open(lockfile, os.O_CREAT | os.O_EXCL)
+        os.open(lockfile,
+                os.O_CREAT | os.O_EXCL)  # create lock file, catch exception if it's already exist
         is_acquired = True
     finally:
-        return is_acquired
+        return is_acquired  # if exception is caught, then some other process is using it
 
 
 def writer(directory: str, filename: str):
     full_name = os.path.join(directory, filename)
     lock_name = full_name + '.lock'
-    data_accumulation = []  # accumulate the data which weren't wroted in file
+    data_accumulation = []  # accumulate the data which weren't written in file before
 
     def write_to_file(data: str):
         data_accumulation.append(data + '\n')
