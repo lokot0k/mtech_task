fa7d604b765d423229ad7e4732939b1ebd544b7e
Локоток <kaptoxa322@mail.ru>
2023-12-11 23:02:57 +0300

    chore: better naming in worker, client service

--- client/worker.py
+++ client/worker.py
@@ -7,12 +7,12 @@ from requests import RequestException
 
 
 class Worker(threading.Thread):
-    def __init__(self, lock, data_generator, write_to_file, delay, url):
+    def __init__(self, lock, data_generator, write_method, delay, url):
         super().__init__()
         self.daemon = True
         self.lock = lock
         self.data_generator = data_generator
-        self.write = write_to_file
+        self.write = write_method
         self.delay = delay
         self.url = url
 
